65aa05196a9b4a88f35f6bb712c32b6813fb6cce
KevinTRiley <kevin.riley.dev@gmail.com>
2014-04-09 08:38:09 -0700

    Creating a Rails Application - Generating the Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'd612ea54619fac9c1a5dd11e91f1fd787862129f5ed2cd6a1235c3532326ede23042b84a0c3b70a5469ccdef2bdb241aa23d51df7d53400f8811d9f4917a8f4a'
